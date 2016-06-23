045e0c2609d456d370808dd7677f282f48acee28
Skullhack <vincent.givaudan@hotmail.fr>
2016-06-23 12:30:54 +0200

    menu haut

--- V 0.0.2/Primaris/menu.h
+++ V 0.0.2/Primaris/menu.h
@@ -1,10 +1,10 @@
 void MenuGauche(SDL_Renderer* renderer){
-
+    rectanglePlein(1, 32, 200, 500, blanc, renderer);
 }
 
 void MenuHaut(SDL_Renderer* renderer){
-    rectanglePlein(1, 1, 100, 50, blanc, renderer);
-    affichetexte("Fichier",20, 5, 12, "JackI.TTF", noir, renderer);
+    rectanglePlein(0, 1, 1366, 30, blanc, renderer);
+    //affichetexte("Fichier", 20, 5, 30, "Arial.ttf", rouge, renderer);
 }
 
 void AffichageMenu(){
